7db8b42a807d4799e136c33fc28847159ba33f1e
Oana <wannawdo@gmail.com>
2022-05-08 16:34:16 +0300

    initial front setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MainPage from './javascript/components/MainPage'
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/javascript/components/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainPage(){
+    return(
+        <div id="MainPage">
+
+        </div>
+    );
+}
+
+export default MainPage;
